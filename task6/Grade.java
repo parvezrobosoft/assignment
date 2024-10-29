8594c09311c4107fc6dc99d4478a316b7067ab41
Mohammad Parvez <mohammad.parvez@robosoftin.com>
2024-10-29 17:36:41 +0530

    used list

--- task6/Grade.java
+++ task6/Grade.java
@@ -6,13 +6,15 @@ class Student
 {
     int id;
     String name;
-    int grades[];
+    float avg;
+    List<Integer> grades;
 
-    Student(int id,String name,int grades[])
+
+    public Student( int id,String name, List<Integer> grades)
     {
-        this.id=id;
-        this.name=name;
-        this.grades=grades;
+        this.name = name;
+        this.id = id;
+        this.grades = grades;
     }
 }
 
@@ -20,19 +22,30 @@ public class Grade {
     public static void main(String[] args) {
 
 
-        ArrayList<Student> students =new ArrayList();
+        ArrayList<Student> students =new ArrayList<>();
 
 
-        Student s1 =new Student(1,"Alice", new int[]{88, 92, 95});
-        Student s2 =new Student(2,"Bob", new int[]{78, 85, 82});
+        Student s1 =new Student(1,"Alice", List.of(88, 92, 95));
+        Student s2 =new Student(2,"Bob", List.of(78, 85, 82));
 
         students.add(s1);
         students.add(s2);
 
-        Map<String, Integer> stringMap=new HashMap<>();
+        Map<String, Float> stringMap=new HashMap<>();
 
-        stringMap.put(s1.name, Arrays.stream(s1.grades).sum()/s1.grades.length);
-        stringMap.put(s2.name, Arrays.stream(s2.grades).sum()/s2.grades.length);
+        for (Student st:students)
+        {
+            int sum=0;
+            for (int i: st.grades)
+            {
+                sum=sum+i;
+            }
+            st.avg=(float)sum/st.grades.size();
+        }
+
+
+        stringMap.put(s1.name,s1.avg);
+        stringMap.put(s2.name, s2.avg);
 
         for (String name:stringMap.keySet())
         {
@@ -50,21 +63,18 @@ public class Grade {
             }
 
 
-            if ((int) stringMap.get(name)<90 && (int) stringMap.get(name)>80)
+            if (stringMap.get(name)<90 &&stringMap.get(name)>80)
             {
                 System.out.println("set B (>80 , <90):");
                 System.out.println(name+" : "+ stringMap.get(name));
             }
 
 
-            if ( (int) stringMap.get(name)<80)
+            if ( stringMap.get(name)<80)
             {
                 System.out.println("set C (>80 , <90):");
                 System.out.println(name+" : "+ stringMap.get(name));
             }
-
-
-
         }
 
     }
